31d83ee42056a3da5139d2f34c3406cecee1d98f
Ellie Lambert <ebezjian@gmail.com>
2020-12-18 12:47:12 -0500

    add logic to ORdersController to work out routing, not functional at this point

--- Tracker/Controllers/OrdersController.cs
+++ Tracker/Controllers/OrdersController.cs
@@ -4,5 +4,32 @@ using System.Collections.Generic;
 
 namespace Tracker.Controllers
 {
-  
+  public class OrdersController : Controllers
+  {
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new
+      Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+
+    [HttpPost("/orders/delete")]
+    public ActionResult DeleteAll()
+    {
+      Order.ClearAll();
+      return View();
+    }
+  }
 }
