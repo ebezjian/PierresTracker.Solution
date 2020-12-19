68cf7cc2bcedcba2a471f7f8d5af06957c5cb2c6
Ellie Lambert <ebezjian@gmail.com>
2020-12-18 19:09:26 -0500

    minor typing fixes to get bug fixed

--- Tracker/Controllers/VendorsController.cs
+++ Tracker/Controllers/VendorsController.cs
@@ -47,7 +47,7 @@ namespace Tracker.Controllers
       foundVendor.AddItem(newOrder);
       List<Order> vendorOrders = foundVendor.Orders;
       model.Add("orders", vendorOrders);
-      model.Add("vendors", foundVendor);
+      model.Add("vendor", foundVendor);
       return View("Show", model);
     }
   }
