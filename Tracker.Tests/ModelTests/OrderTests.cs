9c42db4b6f73f3948aab5d52a056eb91e738364a
Ellie Lambert <ebezjian@gmail.com>
2020-12-20 23:29:51 -0500

    second test passing, to save me time I'll skip the fail part as I did it in previous tests before editing

--- Tracker.Tests/ModelTests/OrderTests.cs
+++ Tracker.Tests/ModelTests/OrderTests.cs
@@ -15,91 +15,15 @@ namespace Tracker.Tests
     }
 
     [TestMethod]
-    public void OrderConstructor_CreatesInstancesofOrder_Order()
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("test");
-      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    Order newOrder = new Order("testTitle", "testDescription", 10, "13/12/2023");
+    Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
-    [TestMethod]
-    public void GetDescription_ReturnsDescription_String()
-    {
-      //Arrange
-      string description = "Coffee beans.";
 
-      //Act
-      Order newOrder = new Order(description);
-      string result = newOrder.Description;
 
-      //Assert
-      Assert.AreEqual(description, result);
-    }
 
-    [TestMethod]
-    public void SetDescription_SetDescription_String()
-    {
-      //Arrange
-      string description = "Cofee Beans.";
-      Order newOrder = new Order(description);
 
-      //Act
-      string updatedDescription = "Coffee Mugs.";
-      newOrder.Description = updatedDescription;
-      string result = newOrder.Description;
-
-      //Assert
-      Assert.AreEqual(updatedDescription, result);
-    }
-    
-    [TestMethod]
-    public void GetAll_ReturnsEmptyList_OrderList()
-    {
-      //Arrange
-      List<Order> newList = new List<Order> { };
-      //Act
-      List<Order> result = Order.GetAll();
-      //Assert
-      CollectionAssert.AreEqual(newList, result);
-    }
-
-    [TestMethod]
-    public void GetAll_ReturnsOrders_OrderList()
-    {
-      //Arrange 
-      string description01 = "Coffee Beans";
-      string description02 = "Coffee Mugs";
-      Order newOrder1 = new Order(description01);
-      Order newOrder2 = new Order(description02);
-      List<Order> newList = new List<Order> {newOrder1, newOrder2};
-      //Act 
-      List<Order> result = Order.GetAll();
-      //Assert
-      CollectionAssert.AreEqual(newList, result);
-    }
-
-    [TestMethod]
-    public void GetId_OrdersInstantiateWithAnIdAndGetterReturns_Int()
-    {
-      //Arrange 
-      string description = "Cofee Beans";
-      Order newOrder = new Order(description);
-      //Act
-      int result = newOrder.Id;
-      //Assert
-      Assert.AreEqual(1, result);
-    }
-    [TestMethod]
-    public void Find_ReturnsCorrectOrder_Order()
-    {
-      //Arrange
-      string description01 = "Coffee Beans";
-      string description02 = "Coffee Mugs";
-      Order newOrder1 = new Order(description01);
-      Order newOrder2 = new Order(description02);
-      //Act
-      Order result = Order.Find(2);
-      //Assert 
-      Assert.AreEqual(newOrder2, result);
-    }
   }
-} 
+}
